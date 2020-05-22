68d0dbb1439e567d125fe6fe5db5fcbc62aa3f33
onurkenis <kenisonur@yandex.com>
2020-05-22 17:33:38 -0500

    feat: migrate push to cordova plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
-import { Plugins } from '@capacitor/core'
+import { Component } from "@angular/core";
+import { Plugins } from "@capacitor/core";
 
-const { HmsPushPlugin, HmsAnalyticsPlugin } = Plugins;
+const { HmsAnalyticsPlugin } = Plugins;
+
+declare var HMSPush: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
-  constructor() { }
+  constructor() {}
 
   getPushToken() {
-    HmsPushPlugin.getToken();
+    HMSPush.getToken(
+      (message) => {
+        // Success callback
+        alert(`Token: ${message}`);
+      },
+      (message) => console.log("Error", message) // Error callback
+    );
   }
 
   logEvent(name: string, bundle: object) {
-    const param = {eventName: name, bundle: JSON.stringify(bundle)}
+    const param = { eventName: name, bundle: JSON.stringify(bundle) };
     HmsAnalyticsPlugin.logEvent(param);
   }
-
 }
